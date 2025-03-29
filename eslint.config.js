c16a469bac63a132d56020a5c39457c6283964f4
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2025-03-29 23:00:18 +0100

    chore: add eslint & prettier

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        node: true,
+      },
+    },
+    ignores: ["node_modules/"],
+  },
+];
