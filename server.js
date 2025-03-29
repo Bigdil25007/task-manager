6f033a2a9fbf0d42fbf37842a5ecd268933dc169
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2025-03-29 23:44:38 +0100

    feat: add get all route

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./config/db");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
 // Connexion à la base de données
 connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
