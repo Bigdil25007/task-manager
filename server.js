bd098b73a269fc862fa8feceded4d8e63253b87f
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2025-03-29 23:49:04 +0100

    chore: change launch message

--- server.js
+++ server.js
@@ -16,4 +16,6 @@ app.use(express.json());
 app.use("/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Serveur démarré à cette addresse: http://localhost:${PORT}`)
+);
