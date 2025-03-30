0a99fcc3c0e6962001abe1724da00ae00455681d
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2025-03-30 07:35:43 +0200

    feat: make a dashboard page

--- routes/webRoutes.js
+++ routes/webRoutes.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/Task");
+const apiClient = require("../config/apiClient");
+
+const {
+  formatDate,
+  getStatusColor,
+  getPriorityColor,
+} = require("../utils/frontendUtils");
 
 // Page d'accueil
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  try {
+    const response = await apiClient.get("/tasks");
+    res.locals.tasks = response.data;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des tâches:", error);
+    res.locals.tasks = [];
+  }
+  res.render("index", {
+    tasks: res.locals.tasks,
+    formatDate,
+    getStatusColor,
+    getPriorityColor,
+  });
 });
 
 module.exports = router;
